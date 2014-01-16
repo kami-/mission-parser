e0f5c332e75cfa7b8ab26332bce37f7b89630603
Laszlo Major <Laszlo_Major@epam.com>
2014-01-16 17:16:22 +0100

    Swaped arguments.

--- js/ast_functions.js
+++ js/ast_functions.js
@@ -9,12 +9,12 @@ MisPars.AstFunctions = MisPars.AstFunctions || {};
         
     };
 
-    AstFunctions.select = function(query, ast) {
+    AstFunctions.select = function(ast, query) {
         var fields = query.split(".");
         var node = ast;
         var result = [];
         for (var i = 0; i < fields.length; i++) {
-            var result = findFieldInNode(fields[i], node);
+            var result = findFieldInNode(node, fields[i]);
             if (result.length == 1) {
                 node = result[0];
             } else if (i !== fields.length - 1) {
@@ -24,25 +24,25 @@ MisPars.AstFunctions = MisPars.AstFunctions || {};
         return result;
     };
     
-    var findFieldInNode = function(fieldName, node) {
+    var findFieldInNode = function(node, fieldName) {
         var result = [];
         if (node.type === Parser.NodeType.ClassField || node.type === Parser.NodeType.Root) {
-            result = findFieldInArray(fieldName, node.fields);
+            result = findFieldInArray(node.fields, fieldName);
         }
         return result;
     };
     
-    var findFieldInArray = function(fieldName, fields) {
+    var findFieldInArray = function(fields, fieldName) {
         var result = [];
         for(var i = 0; i < fields.length; i++) {
-            if (fields[i].value === fieldName || fieldNameStarMatch(fieldName, fields[i].value)) {
+            if (fields[i].value === fieldName || fieldNameStarMatch(fields[i].value, fieldName)) {
                 result.push(fields[i]);
             }
         }
         return result;
     };
 
-    var fieldNameStarMatch = function(fieldName, value) {
+    var fieldNameStarMatch = function(value, fieldName) {
         return fieldName.indexOf("*") === fieldName.length - 1 &&
             value.indexOf(fieldName.substring(0, fieldName.length - 1)) === 0;
     };
